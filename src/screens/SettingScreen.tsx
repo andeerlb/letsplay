9d0e8708b402032870392bfe8e647bcf61e77467
Anderson Babinski <andeerlb@gmail.com>
2025-07-19 19:16:33 -0300

    type for setting

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,6 +1,14 @@
+import { RootStackParamList } from "@components/navigation/rootNavigation";
+import { StackNavigationProp } from "@react-navigation/stack";
 import { Text, View } from "react-native";
 
-export function SettingScreen() {
+type SettingScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Setting'>;
+
+type Props = {
+  navigation: SettingScreenNavigationProp;
+};
+
+export function SettingScreen({ }: Props) {
     return (
         <View><Text>SettingScreen</Text></View>
     )
