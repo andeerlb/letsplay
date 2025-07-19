848459b5d214142a7bf189ed5a8089e264eff46a
Anderson Babinski <andeerlb@gmail.com>
2025-07-19 18:11:26 -0300

    using safearea

--- src/components/navigation/NavigationHeader.tsx
+++ src/components/navigation/NavigationHeader.tsx
@@ -3,6 +3,7 @@ import SettingIcon from '@assets/icons/settings.svg';
 import LetsPlayIcon from '@assets/icons/letsplay.svg';
 import { FontDefinition } from "@constants/theme";
 import { useTheme } from "@context/ThemeContext";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type Props = {
   children: React.ReactNode;
@@ -13,9 +14,16 @@ type Props = {
 
 export default function NavigationHeader({ children, onClick, title, hideSettings = true }: Props) {
   const { theme } = useTheme();
+  const insets = useSafeAreaInsets();
   
   return (
-    <View style={[styles.header, { backgroundColor: theme.secondary.background }]}>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={[styles.header, {
+      paddingTop: insets.top + 15,
+      paddingBottom: 15,
+      paddingHorizontal: 10,
+      backgroundColor: theme.secondary.background
+    }]}>
       <LetsPlayIcon width={50} height={50} />
       {title && <Text style={[styles.title, { color: theme.primary.text }]}>{title}</Text>}
       <View>
@@ -40,9 +48,6 @@ const styles = StyleSheet.create({
   header: {
     display: 'flex',
     flexDirection: 'row',
-    paddingTop: 40,
-    paddingBottom: 10,
-    paddingHorizontal: 20,
     alignItems: 'center',
     justifyContent: 'space-between',
   },
--- src/wrapper/ScreenWrapper.tsx
+++ src/wrapper/ScreenWrapper.tsx
@@ -1,25 +1,18 @@
 import { useTheme } from '@context/ThemeContext';
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function ScreenWrapper({ children }: Props) {
-  const insets = useSafeAreaInsets();
   const { theme } = useTheme();
 
   return (
     <View
       style={[
-        styles.container,
-        {
-          backgroundColor: theme.primary.background,
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom + 8,
-        },
+        styles.container, {backgroundColor: theme.primary.background},
       ]}
     >
       {children}
@@ -30,6 +23,7 @@ export default function ScreenWrapper({ children }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
   }
 });
