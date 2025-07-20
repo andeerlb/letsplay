04a9d7607f7b77fcbd301c15d94f4f367aa8614e
Anderson Babinski <andeerlb@gmail.com>
2025-07-20 17:38:43 -0300

    changed context to redux

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -1,16 +1,24 @@
+// src/hooks/useTheme.ts
 import { useColorScheme } from 'react-native';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
-import { LightTheme, DarkTheme } from '@constants/theme';
+import { LightTheme, DarkTheme, AppTheme } from '@constants/theme';
+import { Layouts } from '../store/slices/themeSlice';
 
-export const useTheme = () => {
+type UseThemeProps = {
+  theme: AppTheme;
+  isDarkMode: boolean;
+  preference: Layouts;
+};
+
+export const useTheme = (): UseThemeProps => {
   const scheme = useColorScheme();
   const preference = useSelector((state: RootState) => state.theme.preference);
 
   const isDarkMode =
     preference === 'dark' || (preference === 'system' && scheme === 'dark');
 
-  const theme = isDarkMode ? DarkTheme : LightTheme;
+  const theme: AppTheme = isDarkMode ? DarkTheme : LightTheme;
 
   return {
     theme,
