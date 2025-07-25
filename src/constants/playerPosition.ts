91ac6b0a080099242cfebff629e5fa568e95e9cf
Anderson Babinski <Anderson Babinski>
2025-07-25 00:28:36 -0300

    saved

---
+++ src/constants/playerPosition.ts
@@ -0,0 +1,34 @@
+import { MessageDescriptor } from "@lingui/core";
+import { msg } from "@lingui/core/macro";
+
+export const PLAYER_POSITIONS_FUT11: MessageDescriptor[] = [
+    msg`player.position.goalkeeper`,
+    msg`player.position.center-back`,
+    msg`player.position.right-back`,
+    msg`player.position.left-back`,
+    msg`player.position.defensive-midfielder`,
+    msg`player.position.central-midfielder`,
+    msg`player.position.attacking-midfielder`,
+    msg`player.position.right-winger`,
+    msg`player.position.left-winger`,
+    msg`player.position.striker`,
+    msg`player.position.second-striker`,
+];
+
+export const PLAYER_POSITIONS_FUT7: MessageDescriptor[] = [
+    msg`player.position.goalkeeper`,
+    msg`player.position.fixo`,
+    msg`player.position.right-wing`,
+    msg`player.position.left-wing`,
+    msg`player.position.midfielder`,
+    msg`player.position.pivot`,
+];
+
+export const PLAYER_POSITIONS_FUTSAL: MessageDescriptor[] = [
+    msg`player.position.goalkeeper`,
+    msg`player.position.fixo`,
+    msg`player.position.right-wing`,
+    msg`player.position.left-wing`,
+    msg`player.position.midfielder`,
+    msg`player.position.pivot`,
+];
--- src/screens/signup/SportScreen.tsx
+++ src/screens/signup/SportScreen.tsx
@@ -3,14 +3,12 @@ import { StyleSheet, Text, View, TextInput } from 'react-native'; // ← TextInp
 import ScreenWrapper from '@wrapper/ScreenWrapper';
 import { useTheme } from '@context/ThemeProvider';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import Input from '@components/input/Input';
 import { Trans, useLingui } from '@lingui/react/macro';
 import { msg, t } from '@lingui/core/macro';
-import Birthdate from '@components/birthdate/Birthdate';
 import Button from '@components/button/Button';
 import { SignUpStackParamList } from '@components/navigation/signUpNavigator';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -40,10 +38,7 @@ export default function SportScreen({ navigation }: { navigation: SportScreenNav
     const { theme } = useTheme();
     const { i18n } = useLingui();
     const safeAreaInsets = useSafeAreaInsets();
-    const description = i18n._(getPlayerExperience(25));
-
-    const surnameRef = useRef<TextInput>(null);
-    const birthdateRef = useRef<TextInput>(null);
+    const description = i18n._(getPlayerExperience(30));
 
     const {
         control,
@@ -86,40 +81,6 @@ export default function SportScreen({ navigation }: { navigation: SportScreenNav
                     </Text>
                 </View>
                 <View style={styles.content}>
-                    <Controller
-                        name="givenName"
-                        control={control}
-                        defaultValue=""
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <Input
-                                label={String(t`screen.signup.given-name`)}
-                                value={value}
-                                onChangeText={onChange}
-                                onBlur={onBlur}
-                                error={errors.givenName?.message}
-                                returnKeyType="next"
-                                onSubmitEditing={() => surnameRef.current?.focus()}
-                            />
-                        )}
-                    />
-                    <Controller
-                        name="surname"
-                        control={control}
-                        defaultValue=""
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <Input
-                                ref={surnameRef}
-                                label={String(t`screen.signup.surname`)}
-                                value={value}
-                                onChangeText={onChange}
-                                onBlur={onBlur}
-                                error={errors.surname?.message}
-                                returnKeyType="next"
-                                onSubmitEditing={() => birthdateRef.current?.focus?.()}
-                            />
-
-                        )}
-                    />
                 </View>
                 <Button label={t`screen.signup.next`} onPress={handleSubmit(onSubmit)} />
             </View>
