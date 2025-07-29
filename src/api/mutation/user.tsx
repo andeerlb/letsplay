b8a4f8cb7a6a0fece8517e6074acbf982cfc5e8c
Anderson Babinski <andeerlb@gmail.com>
2025-07-29 01:38:01 -0300

    apis

--- src/api/mutation/user.tsx
+++ src/api/mutation/user.tsx
@@ -4,7 +4,7 @@ import { UserCredentials } from '@tps/api';
 import { Config } from 'react-native-config';
 
 export const useCreateUser = () => {
-    return useApiPost<UserCredentials, any>(`${Config.AUTH_API}/newplayer`);
+    return useApiPost<UserCredentials, any>(`${Config.API}/public/newplayer`);
 };
 
 export const useGetToken = () => {
