0bd8214f6dd4ed91e70804bdd524316862cd59f1
Anderson Babinski <andeerlb@gmail.com>
2025-07-31 16:03:55 -0300

    improvements to theme contet

--- src/hooks/safeMutation.ts
+++ src/hooks/safeMutation.ts
@@ -4,13 +4,25 @@ import { SilentHandledError } from '@tps/errors';
 export function useSafeMutation<TData = unknown, TError = unknown, TVariables = void>(
     options: UseMutationOptions<TData, TError, TVariables>
 ): UseMutationResult<TData, TError, TVariables> {
+    const wrappedMutationFn = async (variables: TVariables): Promise<TData> => {
+        try {
+            return await options.mutationFn!(variables);
+        } catch (error) {
+            if (error instanceof SilentHandledError) {
+                return new Promise(() => { });
+            }
+            throw error;
+        }
+    };
+
     const wrappedOnError = (error: TError, variables: TVariables, context: unknown) => {
         if (error instanceof SilentHandledError) return;
         options.onError?.(error, variables, context);
     };
 
-    return useMutation({
+    return useMutation<TData, TError, TVariables>({
         ...options,
+        mutationFn: wrappedMutationFn,
         onError: wrappedOnError,
     });
 }
--- src/hooks/safeQuery.ts
+++ src/hooks/safeQuery.ts
@@ -1,12 +1,19 @@
-import { useQuery, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import { QueryKey, useQuery, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
 import { SilentHandledError } from '@tps/errors';
 
-export function useSafeQuery<TQueryFnData, TError = unknown>(
-    options: UseQueryOptions<TQueryFnData, TError>
-): UseQueryResult<TQueryFnData, TError> {
+export function useSafeQuery<
+    TQueryFnData = unknown,
+    TError = unknown,
+    TData = TQueryFnData,
+    TQueryKey extends QueryKey = QueryKey
+>(
+    options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>
+): UseQueryResult<TData, TError> {
     const wrappedOnError = (error: TError) => {
-        if (error instanceof SilentHandledError) return;
         options.onError?.(error);
+        if (error instanceof SilentHandledError) {
+            return new Promise(() => { });
+        }
     };
 
     return useQuery({
